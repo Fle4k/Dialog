cdded05dfbdaaa4973d5554b66fba62b2560a07d
Shahin shokoui <shahin@shahin.de>
2025-05-16 19:44:40 +0200

    Initial Commit

---
+++ Dialog/DialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialogApp.swift
+//  Dialog
+//
+//  Created by Shahin on 16.05.25.
+//
+
+import SwiftUI
+
+@main
+struct DialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DialogTests/DialogTests.swift
@@ -0,0 +1,17 @@
+//
+//  DialogTests.swift
+//  DialogTests
+//
+//  Created by Shahin on 16.05.25.
+//
+
+import Testing
+@testable import Dialog
+
+struct DialogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
